226ec0de816aef83ef8f3b9db448583c2f0fbf48
rajathirumalai <rajaraja16333@gmail.com>
2022-12-13 14:15:27 +0530

    add file

---
+++ condition/main.js
@@ -0,0 +1,14 @@
+var a=prompt("Enter the value");
+if(a>0){
+    console.log("positive number");
+    
+}
+else{
+    if(a<0){
+        console.log("negative number")
+    }
+    else{
+        console.log("neither positive or negative number");
+    }
+    
+}
